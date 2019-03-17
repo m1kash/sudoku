2e625c8e7268b0d3fcca69dd90af8d2e4191e3df
m1kash <yarik.tobolich@gmail.com>
2019-03-18 00:41:52 +0300

    feature: add function decision sudoku

--- src/index.js
+++ src/index.js
@@ -1,3 +1,104 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
-}
+    let matrixSolved = matrix;
+    solve(matrix);
+    function solve (matrix) {
+        let row, col;
+        let objTemp = {
+            'row': 0,
+            'col': 0
+        };
+
+
+        if (findUnassignedLocation(matrix, objTemp)) {
+            return matrix;
+        }
+        row = objTemp['row'];
+        col = objTemp['col'];
+        for (let num = 1; num <= 9; num++) {
+            if (noConflicts(matrix, row, col, num)) {
+                matrixSolved[row][col] = num;
+                console.log(`finded ${matrix[row][col]}`);
+                if (solve(matrix)) {
+                    return true;
+                }
+                matrixSolved[row][col] = 0;
+            }
+        }
+        return false;
+    }
+
+    function findUnassignedLocation(matrix, objTemp) {
+        for (let row = 0; row < 9; row++) {
+            for (let col = 0; col < 9; col++) {
+                if (!matrix[row][col]) {
+                    objTemp['row'] = row;
+                    objTemp['col'] = col;
+                    return;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    function noConflicts(matrix, row, col, num) {
+
+        return isRowOk(matrix, row, num) &&
+            isColOk(matrix, col, num) &&
+            isBoxOk(matrix, row, col, num);
+    }
+
+    function isRowOk(matrix, row, num) {
+        for (let col = 0; col < 9; col++) {
+            console.log(`Is rowOk: ${matrix[row][col]}`);
+            if (matrix[row][col] === num) {
+                return;
+            }
+        }
+
+
+        return true;
+    }
+
+    function isColOk(matrix, col, num) {
+        for (let row = 0; row < 9; row++) {
+            console.log(`Is colOk: ${matrix[row][col]}`);
+            if (matrix[row][col] === num) {
+                return;
+            }
+        }
+
+        return true;
+    }
+
+    function isBoxOk(matrix, row, col, num) {
+        row = Math.floor(row / 3) * 3;
+        col = Math.floor(col / 3) * 3;
+
+        for (let r = 0; r < 3; r++) {
+            for (let c = 0; c < 3; c++) {
+                console.log(`Is isBoxOk: ${matrix[row + r][col + c]}`);
+                if (matrix[row + r][col + c] == num) {
+                    return;
+                }
+            }
+        }
+        return true;
+    }
+
+    return matrixSolved;
+};
+const solveSudoku = require('./index.js');
+const initial = [
+    [0, 0, 4, 0, 5, 0, 0, 0, 0],
+    [3, 5, 0, 0, 0, 0, 6, 9, 7],
+    [6, 7, 0, 0, 0, 0, 0, 0, 0],
+    [4, 0, 0, 6, 8, 0, 0, 0, 0],
+    [0, 6, 0, 0, 0, 0, 0, 8, 0],
+    [0, 8, 0, 5, 0, 0, 3, 0, 0],
+    [0, 3, 0, 9, 0, 0, 7, 0, 5],
+    [0, 4, 0, 8, 0, 0, 0, 0, 9],
+    [0, 0, 0, 0, 0, 3, 0, 1, 0]
+];
+const copy = initial.map(r => [...r]);
+solveSudoku(copy);
